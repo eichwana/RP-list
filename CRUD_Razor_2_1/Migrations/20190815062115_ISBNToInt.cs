2bd8fb4c2cd3fd78c415f0f91e7d627a96c3b1c0
eichwana <tirisfal.guardian@gmail.com>
2019-08-15 17:03:09 +0100

    Add project files.

---
+++ CRUD_Razor_2_1/Migrations/20190815062115_ISBNToInt.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CRUD_Razor_2_1.Migrations
+{
+    public partial class ISBNToInt : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "ISBN",
+                table: "Books",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ISBN",
+                table: "Books",
+                nullable: true,
+                oldClrType: typeof(int));
+        }
+    }
+}
---
+++ CRUD_Razor_2_1/Model/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUD_Razor_2_1.Model
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int ISBN { get; set; }
+        public string Author { get; set; }
+    }
+}
